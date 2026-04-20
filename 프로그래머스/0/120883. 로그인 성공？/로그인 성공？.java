15954ec1d7ff3fe51976f982d260b1f38884a273
junseo090520 <s2543@e-mirim.hs.kr>
2026-04-20 09:04:50 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "";
+        for(int i=0; i<db.length; i++){
+            if(id_pw[0].equals(db[i][0])){
+                answer = "wrong pw";
+                if(id_pw[1].equals(db[i][1])){
+                    answer = "login";
+                }
+            }
+        }
+        if(answer.equals("")){
+            answer = "fail";
+        }
+        return answer;
+    }
+}
