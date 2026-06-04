05f5649c132d09074c4f1cc903380632fd4e280a
junseo090520 <s2543@e-mirim.hs.kr>
2026-06-04 20:47:30 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.06 ms, Memory: 32.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -0,0 +1,9 @@
+function solution(my_string, is_suffix) {
+    var answer = 0;
+    for(let i=0; i<my_string.length; i++){
+        if(my_string.substring(i)===is_suffix){
+            answer=1;
+        }
+    }
+    return answer;
+}
