c9a24c52b041b8bb0262c83698de49a64ca922b7
PARK JUN SEO <junseo090520@gmail.com>
2026-08-26 15:29:19 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
@@ -1,6 +1,4 @@
 def solution(dots):
-    answer = 0
     dots.sort()
-    print(dots)
-    answer = (dots[0][0] - dots[2][0]) * (dots[0][1] - dots[1][1])
+    answer = abs((dots[0][0] - dots[2][0]) * (dots[0][1] - dots[1][1]))
     return answer
